ef5aeed71ed710f4318921e6633f9857c0291003
StarrFox <StarrFox6312@gmail.com>
2023-07-28 06:09:01 +0000

    switch to fabric

--- kubejs/server_scripts/remove_bad_items.js
+++
@@ -1,9 +0,0 @@
-//onEvent("recipes", event => {
-//  event.remove({output: "spelunkery:compression_blast_miner"})
-//  event.remove({output: "easy_villagers:iron_farm"})
-//})
-
-ServerEvents.recipes(event => {
-  //event.remove({output: "spelunkery:compression_blast_miner"})
-  event.remove({output: "easy_villagers:iron_farm"})
-})
